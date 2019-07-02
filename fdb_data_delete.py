1068a1f215c52fe4fc1a054727b936b8296b03bf
andrii <andrii@prism.com>
2019-07-02 12:45:11 -0700

    Add FDB routines

---
+++ fdb_data_delete.py
@@ -0,0 +1,28 @@
+from  datetime import datetime, timedelta
+from multiprocessing import Pool
+import fdb
+from   fdb.tuple import pack, unpack
+import random
+import string
+import uuid
+import logging
+import argparse
+
+fdb.api_version(610)
+
+@fdb.transactional
+def delete_old_records(tr, timeout, bsize):
+    now = str((datetime.utcnow() - timedelta(minutes=timeout)).timestamp())
+    tr.clear_range(pack(('/x00', '')), pack((now,'')))
+
+
+parser = argparse.ArgumentParser(description='FDB data expiration app')
+parser.add_argument('timeout', type=int, help='Data expiration rule in minutes')
+args = parser.parse_args()
+timeout = args.timeout
+bsize = 0
+
+db = fdb.open()
+
+while True:
+    delete_old_records(db, timeout, bsize)
---
+++ fdb_data_generate.py
@@ -0,0 +1,44 @@
+from   datetime import datetime
+from multiprocessing import Pool
+import fdb
+from   fdb.tuple import pack, unpack
+import random
+import string
+import uuid
+import logging
+import argparse
+
+fdb.api_version(610)
+
+def generate_data(N):
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=N))
+
+@fdb.transactional
+def add(tr):
+    data = generate_data(1024).encode('utf-8')
+    now = str(datetime.utcnow().timestamp())
+    uid = str(uuid.uuid4().hex)
+    key = pack((now, uid))
+    tr.set(key, data)
+
+def add_batch_process(lim):
+    db = fdb.open()
+    i = 0
+    while i < lim if lim else 1:
+        try:
+            add(db)
+        except Exception:
+            logging.exception('')
+        if lim:
+            i = i + 1
+
+parser = argparse.ArgumentParser(description='FDB data generation app')
+parser.add_argument('num_threads', type=int, help='Number of threads to run data generation')
+parser.add_argument('--num_records', type=int, default=None, help='Records per thread, if not set will run indefinitely')
+args = parser.parse_args()
+num_proc = args.num_threads
+num_records = args.num_records
+
+p = Pool(num_proc)
+multiple_results = [p.apply_async(add_batch_process, (num_records,)) for i in range(num_proc)]
+[res.get(timeout=None) for res in multiple_results]
