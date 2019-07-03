1078f1dd5c93ca77f5eacff6f82d39ffe44fac15
andrii <andrii@prism.com>
2019-07-03 14:50:48 -0700

    Add data size param

--- citus_data_generate.py
+++ citus_data_generate.py
@@ -11,21 +11,21 @@ import psycopg2
 def generate_data(N):
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=N))
 
-def add(conn, bsize):
+def add(conn, bsize, dsize):
     with conn.cursor() as curs:
         for i in range(bsize):
-            data = generate_data(1024)
+            data = generate_data(dsize)
             uid = str(uuid.uuid4().hex)
             SQL = "INSERT INTO a00 (FEED_ID, CREATED, LABEL) VALUES (1, now(), '{}'::text);".format(data)
             curs.execute(SQL)
         conn.commit()
 
-def add_batch_process(conn_string, lim, bsize):
+def add_batch_process(conn_string, lim, bsize, dsize):
     conn = psycopg2.connect(conn_string)
     i = 0
     while i < lim if lim else 1:
         try:
-            add(conn, bsize)
+            add(conn, bsize, dsize)
         except Exception:
             logging.exception('')
         if lim:
@@ -37,16 +37,18 @@ parser.add_argument('port', type=int, help='PostgreSQL server port')
 parser.add_argument('dbname', type=str, help='DB name')
 parser.add_argument('num_threads', type=int, help='Number of threads to run data generation')
 parser.add_argument('batch_size', type=int, help='Commit batch size')
+parser.add_argument('data_size', type=int, help='Data buffer size (bytes)')
 parser.add_argument('--num_records', type=int, default=None, help='Records per thread, if not set will run indefinitely')
 args = parser.parse_args()
 num_proc = args.num_threads
 num_records = args.num_records
 batch_size = args.batch_size
+data_size = args.data_size
 conn_string = "port='{port}' dbname='{dbname}'".format(
     port=args.port,
     dbname=args.dbname,
 )
 
 p = Pool(num_proc)
-multiple_results = [p.apply_async(add_batch_process, (conn_string, num_records, batch_size,)) for i in range(num_proc)]
+multiple_results = [p.apply_async(add_batch_process, (conn_string, num_records, batch_size, data_size,)) for i in range(num_proc)]
 [res.get(timeout=None) for res in multiple_results]
