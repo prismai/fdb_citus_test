aab669459e3bd56a08713087429df2096ccda55b
andrii <andrii@prism.com>
2019-07-02 19:50:53 -0700

    Fix citus create script

--- citus_data_generate.py
+++ citus_data_generate.py
@@ -13,14 +13,14 @@ def generate_data(N):
 
 def add(conn):
     data = generate_data(1024)
-    now = str(datetime.utcnow().timestamp())
     uid = str(uuid.uuid4().hex)
-    SQL = "INSERT INTO a00 (FEED_ID, CREATED, LABEL) VALUES (1, '{}'::timestamp, '{}'::text)".format(now, data)
+    SQL = "INSERT INTO a00 (FEED_ID, CREATED, LABEL) VALUES (1, now(), '{}'::text);".format(data)
     with conn.cursor() as curs:
         curs.execute(SQL)
+        conn.commit()
 
 def add_batch_process(conn_string, lim):
-	conn = psycopg2.connect(conn_string)
+    conn = psycopg2.connect(conn_string)
     i = 0
     while i < lim if lim else 1:
         try:
@@ -29,24 +29,19 @@ def add_batch_process(conn_string, lim):
             logging.exception('')
         if lim:
             i = i + 1
+    conn.close()
 
 parser = argparse.ArgumentParser(description='FDB data generation app')
-parser.add_argument('host', type=int, help='PostgreSQL server hostname')
 parser.add_argument('port', type=int, help='PostgreSQL server port')
 parser.add_argument('dbname', type=int, help='DB name')
-parser.add_argument('user', type=int, help='DB user')
-parser.add_argument('pwd', type=int, help='DB password')
 parser.add_argument('num_threads', type=int, help='Number of threads to run data generation')
 parser.add_argument('--num_records', type=int, default=None, help='Records per thread, if not set will run indefinitely')
 args = parser.parse_args()
 num_proc = args.num_threads
 num_records = args.num_records
-conn_string = "host='{host}' port='{port}' dbname='{dbname}' user='{user}' password={pwd}".format(
-    host = args.host,
-    port = args.port,
-    dbname = args.dbname,
-    user = args.user,
-    pwd = args.pwd
+conn_string = "port='{port}' dbname='{dbname}'".format(
+    port=args.port,
+    dbname=args.dbname,
 )
 
 p = Pool(num_proc)
