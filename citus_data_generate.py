98fde635bae759ae8fd69b64d5dacca74f9c86f4
andrii <andrii@prism.com>
2019-07-03 15:04:04 -0700

    Add data randomization

--- citus_data_generate.py
+++ citus_data_generate.py
@@ -6,26 +6,27 @@ import uuid
 import logging
 import argparse
 import psycopg2
+import random
 
 
 def generate_data(N):
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=N))
 
-def add(conn, bsize, dsize):
+def add(conn, bsize, dsize, rand_size):
     with conn.cursor() as curs:
         for i in range(bsize):
-            data = generate_data(dsize)
+            data = generate_data(random.randint(1, dsize))
             uid = str(uuid.uuid4().hex)
             SQL = "INSERT INTO a00 (FEED_ID, CREATED, LABEL) VALUES (1, now(), '{}'::text);".format(data)
             curs.execute(SQL)
         conn.commit()
 
-def add_batch_process(conn_string, lim, bsize, dsize):
+def add_batch_process(conn_string, lim, bsize, dsize, rand_size):
     conn = psycopg2.connect(conn_string)
     i = 0
     while i < lim if lim else 1:
         try:
-            add(conn, bsize, dsize)
+            add(conn, bsize, dsize, rand_size)
         except Exception:
             logging.exception('')
         if lim:
@@ -38,17 +39,19 @@ parser.add_argument('dbname', type=str, help='DB name')
 parser.add_argument('num_threads', type=int, help='Number of threads to run data generation')
 parser.add_argument('batch_size', type=int, help='Commit batch size')
 parser.add_argument('data_size', type=int, help='Data buffer size (bytes)')
+parser.add_argument('--random_dsize', action='store_true', default=False)
 parser.add_argument('--num_records', type=int, default=None, help='Records per thread, if not set will run indefinitely')
 args = parser.parse_args()
 num_proc = args.num_threads
 num_records = args.num_records
 batch_size = args.batch_size
 data_size = args.data_size
+rand_size = args.random_dsize
 conn_string = "port='{port}' dbname='{dbname}'".format(
     port=args.port,
     dbname=args.dbname,
 )
 
 p = Pool(num_proc)
-multiple_results = [p.apply_async(add_batch_process, (conn_string, num_records, batch_size, data_size,)) for i in range(num_proc)]
+multiple_results = [p.apply_async(add_batch_process, (conn_string, num_records, batch_size, data_size, rand_size,)) for i in range(num_proc)]
 [res.get(timeout=None) for res in multiple_results]
--- fdb_data_generate.py
+++ fdb_data_generate.py
@@ -7,6 +7,7 @@ import string
 import uuid
 import logging
 import argparse
+import random
 
 fdb.api_version(610)
 
@@ -14,20 +15,20 @@ def generate_data(N):
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=N))
 
 @fdb.transactional
-def add(tr, bsize, dsize):
+def add(tr, bsize, dsize, rand_size):
     for i in range(bsize):
-        data = generate_data(dsize).encode('utf-8')
+        data = generate_data(random.randint(1, dsize)).encode('utf-8')
         now = str(datetime.utcnow().timestamp())
         uid = str(uuid.uuid4().hex)
         key = pack((now, uid))
         tr.set(key, data)
 
-def add_batch_process(lim, bsize, dsize):
+def add_batch_process(lim, bsize, dsize, rand_size):
     db = fdb.open()
     i = 0
     while i < lim if lim else 1:
         try:
-            add(db, bsize, dsize)
+            add(db, bsize, dsize, rand_size)
         except Exception:
             logging.exception('')
         if lim:
@@ -37,13 +38,15 @@ parser = argparse.ArgumentParser(description='FDB data generation app')
 parser.add_argument('num_threads', type=int, help='Number of threads to run data generation')
 parser.add_argument('batch_size', type=int, help='Commit batch size')
 parser.add_argument('data_size', type=int, help='Data buffer size (bytes)')
+parser.add_argument('--random_dsize', action='store_true', default=False)
 parser.add_argument('--num_records', type=int, default=None, help='Records per thread, if not set will run indefinitely')
 args = parser.parse_args()
 num_proc = args.num_threads
 num_records = args.num_records
 batch_size = args.batch_size
 data_size = args.data_size
+rand_size = args.random_dsize
 
 p = Pool(num_proc)
-multiple_results = [p.apply_async(add_batch_process, (num_records, batch_size, data_size,)) for i in range(num_proc)]
+multiple_results = [p.apply_async(add_batch_process, (num_records, batch_size, data_size, rand_size)) for i in range(num_proc)]
 [res.get(timeout=None) for res in multiple_results]
