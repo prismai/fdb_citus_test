15ebcd6aebb456209d74fca74c5a1ead2ae0b0fb
andrii <andrii@prism.com>
2019-07-03 10:55:09 -0700

    Add batch insert

--- citus_data_generate.py
+++ citus_data_generate.py
@@ -11,20 +11,21 @@ import psycopg2
 def generate_data(N):
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=N))
 
-def add(conn):
-    data = generate_data(1024)
-    uid = str(uuid.uuid4().hex)
-    SQL = "INSERT INTO a00 (FEED_ID, CREATED, LABEL) VALUES (1, now(), '{}'::text);".format(data)
+def add(conn, bsize):
     with conn.cursor() as curs:
-        curs.execute(SQL)
+        for i in range(bsize):
+            data = generate_data(1024)
+            uid = str(uuid.uuid4().hex)
+            SQL = "INSERT INTO a00 (FEED_ID, CREATED, LABEL) VALUES (1, now(), '{}'::text);".format(data)
+            curs.execute(SQL)
         conn.commit()
 
-def add_batch_process(conn_string, lim):
+def add_batch_process(conn_string, lim, bsize):
     conn = psycopg2.connect(conn_string)
     i = 0
     while i < lim if lim else 1:
         try:
-            add(conn)
+            add(conn, bsize)
         except Exception:
             logging.exception('')
         if lim:
@@ -35,15 +36,17 @@ parser = argparse.ArgumentParser(description='FDB data generation app')
 parser.add_argument('port', type=int, help='PostgreSQL server port')
 parser.add_argument('dbname', type=str, help='DB name')
 parser.add_argument('num_threads', type=int, help='Number of threads to run data generation')
+parser.add_argument('batch_size', type=int, help='Commit batch size')
 parser.add_argument('--num_records', type=int, default=None, help='Records per thread, if not set will run indefinitely')
 args = parser.parse_args()
 num_proc = args.num_threads
 num_records = args.num_records
+batch_size = args.batch_size
 conn_string = "port='{port}' dbname='{dbname}'".format(
     port=args.port,
     dbname=args.dbname,
 )
 
 p = Pool(num_proc)
-multiple_results = [p.apply_async(add_batch_process, (conn_string, num_records,)) for i in range(num_proc)]
+multiple_results = [p.apply_async(add_batch_process, (conn_string, num_records, batch_size,)) for i in range(num_proc)]
 [res.get(timeout=None) for res in multiple_results]
--- fdb_data_generate.py
+++ fdb_data_generate.py
@@ -14,19 +14,20 @@ def generate_data(N):
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=N))
 
 @fdb.transactional
-def add(tr):
-    data = generate_data(1024).encode('utf-8')
-    now = str(datetime.utcnow().timestamp())
-    uid = str(uuid.uuid4().hex)
-    key = pack((now, uid))
-    tr.set(key, data)
+def add(tr, bsize):
+    for i in range(bsize):
+        data = generate_data(1024).encode('utf-8')
+        now = str(datetime.utcnow().timestamp())
+        uid = str(uuid.uuid4().hex)
+        key = pack((now, uid))
+        tr.set(key, data)
 
-def add_batch_process(lim):
+def add_batch_process(lim, bsize):
     db = fdb.open()
     i = 0
     while i < lim if lim else 1:
         try:
-            add(db)
+            add(db, bsize)
         except Exception:
             logging.exception('')
         if lim:
@@ -34,11 +35,13 @@ def add_batch_process(lim):
 
 parser = argparse.ArgumentParser(description='FDB data generation app')
 parser.add_argument('num_threads', type=int, help='Number of threads to run data generation')
+parser.add_argument('batch_size', type=int, help='Commit batch size')
 parser.add_argument('--num_records', type=int, default=None, help='Records per thread, if not set will run indefinitely')
 args = parser.parse_args()
 num_proc = args.num_threads
 num_records = args.num_records
+batch_size = args.batch_size
 
 p = Pool(num_proc)
-multiple_results = [p.apply_async(add_batch_process, (num_records,)) for i in range(num_proc)]
+multiple_results = [p.apply_async(add_batch_process, (num_records, batch_size,)) for i in range(num_proc)]
 [res.get(timeout=None) for res in multiple_results]
